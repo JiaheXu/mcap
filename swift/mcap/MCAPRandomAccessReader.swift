368895de1a99ba5e9e5d761a61925dcc8439db7c
Jiahe Xu <48428720+JiaheXu@users.noreply.github.com>
2025-02-08 16:31:29 -0500

    Update MCAPRandomAccessReader.swift

--- swift/mcap/MCAPRandomAccessReader.swift
+++ swift/mcap/MCAPRandomAccessReader.swift
@@ -77,7 +77,7 @@ class ChunkCursor: Comparable {
 
   var sortTime: UInt64 {
     if let messageIndexCursors = messageIndexCursors {
-      let cursor = messageIndexCursors.min()!
+      let cursor = messageIndexCursors!
       return cursor.records[cursor.index].logTime
     }
     return chunkIndex.messageStartTime
